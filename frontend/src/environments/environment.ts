f3957dc42c4437ca37e13b039d4b4cc7f2f13ce5
miharizo17 <kantomiharizo@gmail.com>
2025-04-02 20:15:44 +0300

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ import packageInfo from '../../package.json';
 
 export const environment = {
   appVersion: packageInfo.version,
-  production: true,
+  production: false,
   apiUrl: 'https://m1p12mean-kanto-soatiana-2.onrender.com' // URL pour le développement
 };
 
