d8c33db5cd4e6d6ea908eaa406f68b1010fbad3a
miharizo17 <kantomiharizo@gmail.com>
2025-04-02 21:10:57 +0300

    Update annee-voiture.service.ts

--- frontend/src/app/services/Manager/annee-voiture.service.ts
+++ frontend/src/app/services/Manager/annee-voiture.service.ts
@@ -19,33 +19,33 @@ export class AnneeVoitureService {
   // Liste des années
   getAnnee(): Observable<any> {
     console.log("Headers : ", this.headers);
-    return this.http.get(this.apiUrl, { headers: this.headers });
+    return this.http.get(this.apiUrl, {   headers: this.headers, observe: 'response' });
   }
 
   // Créer une nouvelle année
   addAnnee(annee: string, note: string): Observable<any> {
     const body = { annee, note };
-    return this.http.post(this.apiUrl, body, { headers: this.headers });
+    return this.http.post(this.apiUrl, body, {   headers: this.headers, observe: 'response' });
   }
 
   // Mettre à jour une année
   updateAnnee(id: string, annee: string, note: string): Observable<any> {
     const body = { annee, note };
-    return this.http.put(`${this.apiUrl}/${id}`, body, { headers: this.headers });
+    return this.http.put(`${this.apiUrl}/${id}`, body, {   headers: this.headers, observe: 'response' });
   }
 
   // Supprimer une année
   deleteAnnee(id: string): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/${id}`, { headers: this.headers });
+    return this.http.delete(`${this.apiUrl}/${id}`, {   headers: this.headers, observe: 'response' });
   }
 
   // Recherche d'une année
   searchAnnee(query: string): Observable<any> {
-    return this.http.get(`${this.apiUrlSearch}?q=${query}`, { headers: this.headers });
+    return this.http.get(`${this.apiUrlSearch}?q=${query}`, {   headers: this.headers, observe: 'response' });
   }
 
   // Récupérer une année par ID
   getAnneeById(id: string): Observable<any> {
-    return this.http.get(`${this.apiUrlById}/${id}`, { headers: this.headers });
+    return this.http.get(`${this.apiUrlById}/${id}`, {   headers: this.headers, observe: 'response' });
   }
 }
