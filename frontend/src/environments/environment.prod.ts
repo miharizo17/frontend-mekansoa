1997b5cbf15d655c65574dd435407722825a99ac
miharizo17 <kantomiharizo@gmail.com>
2025-04-04 01:10:47 +0300

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl: 'https://mekansoa-backend.onrender.com/'
+  apiUrl: 'https://mekansoa-backend.onrender.com'
 };
